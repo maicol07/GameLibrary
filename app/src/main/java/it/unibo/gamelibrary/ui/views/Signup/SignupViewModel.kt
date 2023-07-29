c6da69d64277493f6149eb4454f6789533b4cc3c
Maicol Battistini <maicolbattistini@live.it>
2023-07-29 11:03:44 +0200

    refactor: ♻️ Refactor context activity lookup into utility class
    The function findActivity() that was previously located within the SignupViewModel has now been moved to a separate utility class for better organisation and reusability. This function, that finds the activity context from a given context, is not solely tied to the scope of the SignupViewModel and thus was segregated to an external utility class.

--- app/src/main/java/it/unibo/gamelibrary/ui/views/Signup/SignupViewModel.kt
+++ app/src/main/java/it/unibo/gamelibrary/ui/views/Signup/SignupViewModel.kt
@@ -1,8 +1,5 @@
 package it.unibo.gamelibrary.ui.views.Signup
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
@@ -22,15 +19,6 @@ import it.unibo.gamelibrary.ui.views.destinations.HomeDestination
 import it.unibo.gamelibrary.utils.snackbarHostState
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-fun Context.findActivity(): Activity {
-    var context = this
-    while (context is ContextWrapper) {
-        if (context is Activity) return context
-        context = context.baseContext
-    }
-    throw IllegalStateException("no activity")
-}
 
 @HiltViewModel
 class SignupViewModel @Inject constructor(
--- app/src/main/java/it/unibo/gamelibrary/utils/utils.kt
+++ app/src/main/java/it/unibo/gamelibrary/utils/utils.kt
@@ -1,5 +1,8 @@
 package it.unibo.gamelibrary.utils
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.compose.material3.SnackbarHostState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -8,4 +11,13 @@ val snackbarHostState = SnackbarHostState()
 
 suspend fun <T> IGDBApiRequest(apiRequest: () -> T): T = withContext(Dispatchers.IO) {
     apiRequest()
+}
+
+fun Context.findActivity(): Activity {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    throw IllegalStateException("no activity")
 }
