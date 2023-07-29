93c36ab2c6517e6fae59b3b1d1fd57aabaf50c7d
Maicol Battistini <maicolbattistini@live.it>
2023-07-29 10:56:17 +0200

    feat: ✨ Add CustomDialog component
    Created a new CustomDialog.kt file under the ui.common.components package to support more customizability in dialogs. This component allows customizable elements for different parts of the dialog such as buttons, icons, and text styles. Also, it gives flexibility to tweak the color scheme, padding, and alignments. This is a useful utility for providing a consistent look and feel to dialogs across the application.

---
+++ app/src/main/java/it/unibo/gamelibrary/ui/common/components/CustomDialog.kt
@@ -0,0 +1,229 @@
+package it.unibo.gamelibrary.ui.common.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlin.math.max
+
+// Paddings for each of the dialog's parts.
+private val DialogPadding = PaddingValues(all = 24.dp)
+private val IconPadding = PaddingValues(bottom = 16.dp)
+private val TitlePadding = PaddingValues(bottom = 16.dp)
+private val TextPadding = PaddingValues(bottom = 24.dp)
+private val ButtonsMainAxisSpacing = 8.dp
+private val ButtonsCrossAxisSpacing = 12.dp
+
+/**
+ * Displays a custom dialog with specified elements and styling.
+ *
+ * @param onDismissRequest The callback to be invoked when the dialog is dismissed.
+ * @param modifier The modifier for the dialog.
+ * @param buttons The composable content for the buttons in the dialog.
+ * @param icon The composable content for the icon in the dialog.
+ * @param title The composable content for the title in the dialog.
+ * @param shape The shape of the dialog.
+ * @param containerColor The color of the dialog container.
+ * @param iconContentColor The color of the icon content in the dialog.
+ * @param titleContentColor The color of the title content in the dialog.
+ * @param textContentColor The color of the text content in the dialog.
+ * @param buttonContentColor The color of the button content in the dialog.
+ * @param tonalElevation The tonal elevation of the dialog.
+ * @param content The composable content for the main content in the dialog.
+ *
+ * @see AlertDialog
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDialog(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    buttons: @Composable (() -> Unit)? = null,
+    icon: @Composable (() -> Unit)? = null,
+    title: @Composable (() -> Unit)? = null,
+    shape: Shape = AlertDialogDefaults.shape,
+    containerColor: Color = AlertDialogDefaults.containerColor,
+    iconContentColor: Color = AlertDialogDefaults.iconContentColor,
+    titleContentColor: Color = AlertDialogDefaults.titleContentColor,
+    textContentColor: Color = AlertDialogDefaults.textContentColor,
+    buttonContentColor: Color = MaterialTheme.colorScheme.primary,
+    tonalElevation: Dp = AlertDialogDefaults.TonalElevation,
+    content: @Composable (() -> Unit),
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismissRequest,
+    ) {
+        Surface(
+            modifier = modifier,
+            shape = shape,
+            color = containerColor,
+            tonalElevation = tonalElevation,
+        ) {
+            Column(
+                modifier = Modifier.padding(DialogPadding)
+            ) {
+                icon?.let {
+                    CompositionLocalProvider(LocalContentColor provides iconContentColor) {
+                        Box(
+                            Modifier
+                                .padding(IconPadding)
+                                .align(Alignment.CenterHorizontally)
+                        ) {
+                            icon()
+                        }
+                    }
+                }
+                title?.let {
+                    CompositionLocalProvider(LocalContentColor provides titleContentColor) {
+                        val textStyle = MaterialTheme.typography.headlineSmall
+                        ProvideTextStyle(textStyle) {
+                            Box(
+                                // Align the title to the center when an icon is present.
+                                Modifier
+                                    .padding(TitlePadding)
+                                    .align(
+                                        if (icon == null) {
+                                            Alignment.Start
+                                        } else {
+                                            Alignment.CenterHorizontally
+                                        }
+                                    )
+                            ) {
+                                title()
+                            }
+                        }
+                    }
+                }
+                CompositionLocalProvider(LocalContentColor provides textContentColor) {
+                    val textStyle =
+                        MaterialTheme.typography.bodyMedium
+                    ProvideTextStyle(textStyle) {
+                        Box(
+                            Modifier
+                                .weight(weight = 1f, fill = false)
+                                .padding(TextPadding)
+                                .align(Alignment.Start)
+                        ) {
+                            content()
+                        }
+                    }
+                }
+                buttons?.let {
+                    Box(modifier = Modifier.align(Alignment.End)) {
+                        CompositionLocalProvider(LocalContentColor provides buttonContentColor) {
+                            val textStyle =
+                                MaterialTheme.typography.labelLarge
+                            ProvideTextStyle(value = textStyle, content = {
+                                AlertDialogFlowRow(
+                                    mainAxisSpacing = ButtonsMainAxisSpacing,
+                                    crossAxisSpacing = ButtonsCrossAxisSpacing
+                                ) {
+                                    buttons()
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AlertDialogFlowRow(
+    mainAxisSpacing: Dp,
+    crossAxisSpacing: Dp,
+    content: @Composable () -> Unit
+) {
+    Layout(content) { measurables, constraints ->
+        val sequences = mutableListOf<List<Placeable>>()
+        val crossAxisSizes = mutableListOf<Int>()
+        val crossAxisPositions = mutableListOf<Int>()
+
+        var mainAxisSpace = 0
+        var crossAxisSpace = 0
+
+        val currentSequence = mutableListOf<Placeable>()
+        var currentMainAxisSize = 0
+        var currentCrossAxisSize = 0
+
+        // Return whether the placeable can be added to the current sequence.
+        fun canAddToCurrentSequence(placeable: Placeable) =
+            currentSequence.isEmpty() || currentMainAxisSize + mainAxisSpacing.roundToPx() +
+                    placeable.width <= constraints.maxWidth
+
+        // Store current sequence information and start a new sequence.
+        fun startNewSequence() {
+            if (sequences.isNotEmpty()) {
+                crossAxisSpace += crossAxisSpacing.roundToPx()
+            }
+            // Ensures that confirming actions appear above dismissive actions.
+            sequences.add(0, currentSequence.toList())
+            crossAxisSizes += currentCrossAxisSize
+            crossAxisPositions += crossAxisSpace
+
+            crossAxisSpace += currentCrossAxisSize
+            mainAxisSpace = max(mainAxisSpace, currentMainAxisSize)
+
+            currentSequence.clear()
+            currentMainAxisSize = 0
+            currentCrossAxisSize = 0
+        }
+
+        for (measurable in measurables) {
+            // Ask the child for its preferred size.
+            val placeable = measurable.measure(constraints)
+
+            // Start a new sequence if there is not enough space.
+            if (!canAddToCurrentSequence(placeable)) startNewSequence()
+
+            // Add the child to the current sequence.
+            if (currentSequence.isNotEmpty()) {
+                currentMainAxisSize += mainAxisSpacing.roundToPx()
+            }
+            currentSequence.add(placeable)
+            currentMainAxisSize += placeable.width
+            currentCrossAxisSize = max(currentCrossAxisSize, placeable.height)
+        }
+
+        if (currentSequence.isNotEmpty()) startNewSequence()
+
+        val mainAxisLayoutSize = max(mainAxisSpace, constraints.minWidth)
+
+        val crossAxisLayoutSize = max(crossAxisSpace, constraints.minHeight)
+
+        val layoutWidth = mainAxisLayoutSize
+
+        val layoutHeight = crossAxisLayoutSize
+
+        layout(layoutWidth, layoutHeight) {
+            sequences.forEachIndexed { i, placeables ->
+                val childrenMainAxisSizes = IntArray(placeables.size) { j ->
+                    placeables[j].width +
+                            if (j < placeables.lastIndex) mainAxisSpacing.roundToPx() else 0
+                }
+                val arrangement = Arrangement.End
+                val mainAxisPositions = IntArray(childrenMainAxisSizes.size) { 0 }
+                with(arrangement) {
+                    arrange(mainAxisLayoutSize, childrenMainAxisSizes,
+                        layoutDirection, mainAxisPositions)
+                }
+                placeables.forEachIndexed { j, placeable ->
+                    placeable.place(
+                        x = mainAxisPositions[j],
+                        y = crossAxisPositions[i]
+                    )
+                }
+            }
+        }
+    }
+}
