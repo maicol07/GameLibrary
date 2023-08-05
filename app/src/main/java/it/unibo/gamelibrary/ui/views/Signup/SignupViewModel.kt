2faccafa791842f446d8d13153582ec59043002f
Maicol Battistini <maicolbattistini@live.it>
2023-08-05 12:44:11 +0200

    refactor: Remove unused code and make isSignupButtonPressed private
    This commit removes several lines of unnecessary code that were commented out and not being used within the SignupViewModel. This cleanup helps to improve code readability. Also, isSignupButtonPressed has been made private for better encapsulation and to ensure it is not mistakenly modified from outside.

--- app/src/main/java/it/unibo/gamelibrary/ui/views/Signup/SignupViewModel.kt
+++ app/src/main/java/it/unibo/gamelibrary/ui/views/Signup/SignupViewModel.kt
@@ -25,9 +25,7 @@ class SignupViewModel @Inject constructor(
     private val repository: UserRepository
 ) : ViewModel() {
 
-    //lateinit var fusedLocationClient: FusedLocationProviderClient
     private val auth: FirebaseAuth = Firebase.auth
-    //val addressField = mutableStateListOf<Double>()
     var fieldsErrors = mutableStateMapOf(
         Pair("name", false),
         Pair("surname", false),
@@ -46,93 +44,10 @@ class SignupViewModel @Inject constructor(
         Pair("password", ""),
         Pair("confirmPassword", "")
     )
-    //var address: Address? = null
-
-    //var isLocalizationStarted = mutableStateOf(false)
-    //var isLocalizationFailed = mutableStateOf(false)
-    var isSignupButtonPressed by mutableStateOf(false)
+    private var isSignupButtonPressed by mutableStateOf(false)
 
     var isPasswordHidden by mutableStateOf(true)
     var isPasswordConfirmHidden by mutableStateOf(true)
-
-    //var isPermissionGranted = mutableStateOf(false)
-    //var isDialogOpen = mutableStateOf(false)
-
-    /*fun getCurrentPosition(@ActivityContext activityContext: Context) {
-        val activity = activityContext.findActivity() as MainActivity
-        isLocalizationStarted.value = true
-        Toast.makeText(activityContext, "Start Location", Toast.LENGTH_SHORT).show()
-        if (ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(activityContext, "Permissions denied", Toast.LENGTH_SHORT).show()
-            return
-        }
-        fusedLocationClient.getCurrentLocation(
-            Priority.PRIORITY_HIGH_ACCURACY,
-            object : CancellationToken() {
-                override fun onCanceledRequested(p0: OnTokenCanceledListener) =
-                    CancellationTokenSource().token
-
-                override fun isCancellationRequested() = false
-            }).addOnSuccessListener { location: Location? ->
-            if (location == null) {
-                Toast.makeText(activity, "Cannot get location.", Toast.LENGTH_SHORT).show()
-            } else {
-                addressField.clear()
-                addressField.addAll(listOf(location.latitude, location.longitude))
-                Log.d("LOCATION", "${location.latitude}, ${location.longitude}")
-            }
-            isLocalizationStarted.value = false
-            isLocalizationFailed.value = false
-        }
-    }*/
-
-    /*fun getLocation(
-        @ApplicationContext context: Context,
-        onGeocode: (addressName: Address) -> Unit
-    ) {
-        if (Build.VERSION.SDK_INT >= 33) {
-            Geocoder(
-                context,
-                Locale.getDefault()
-            ).getFromLocation(
-                addressField.toDoubleArray()[0],
-                addressField.toDoubleArray()[1],
-                1,
-                object : Geocoder.GeocodeListener {
-                    override fun onGeocode(addresses: MutableList<Address>) {
-                        Log.d("ADDRESS", addresses[0].toString())
-                        addressField.clear()
-                        onGeocode.invoke(addresses[0])
-                    }
-
-                    override fun onError(errorMessage: String?) {
-                        super.onError(errorMessage)
-                        isLocalizationFailed.value = true
-                    }
-                }
-            )
-        } else {
-            @Suppress("DEPRECATION")
-            val address = Geocoder(
-                context,
-                Locale.getDefault()
-            ).getFromLocation(
-                addressField.toDoubleArray()[0],
-                addressField.toDoubleArray()[1],
-                1
-            )?.get(0)
-            if (address != null) {
-                onGeocode.invoke(address)
-            }
-        }
-    }*/
 
     fun signUp(navController: NavController) {
         if (!isSignupButtonPressed) {
@@ -151,7 +66,6 @@ class SignupViewModel @Inject constructor(
                                 fields["surname"]!!,
                                 fields["username"]!!,
                                 fields["email"]!!
-                                //"${address?.latitude} ${address?.longitude}"
                             )
                             insertUser(user)
                             navController.navigate(HomeDestination())
