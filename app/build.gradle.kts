b98d849e2093a5a13b22a727419e5f5f59cd856f
Maicol Battistini <maicolbattistini@live.it>
2023-08-05 20:39:22 +0200

    chore(deps): ⬆️ Upgraded dependencies and switched to Gradle version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,13 @@
 @file:Suppress("UnstableApiUsage")
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
-    id("com.google.gms.google-services")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.gms)
     kotlin("kapt")
-    id("com.google.dagger.hilt.android")
-    id("com.klaxit.hiddensecrets") version "0.2.1"
+    alias(libs.plugins.hiddenSecrets)
 }
 
 android {
@@ -18,14 +17,12 @@ android {
     defaultConfig {
         applicationId = "it.unibo.gamelibrary"
         minSdk = 26
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion (AGP 8.1 required)
+        targetSdk = 34
         versionCode = rootProject.extra["versionCode"] as Int
         versionName = rootProject.extra["versionName"] as String
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -47,10 +44,9 @@ android {
     }
     buildFeatures {
         compose = true
-        viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.8"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     // Enable NDK build
     externalNativeBuild {
@@ -58,92 +54,62 @@ android {
             path("src/main/cpp/CMakeLists.txt")
         }
     }
-    ndkVersion = "25.1.8937393"
-    buildToolsVersion = "33.0.2"
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("com.github.skydoves:landscapist-glide:2.2.5")
-    implementation("com.github.skydoves:landscapist-animation:2.2.5")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("io.github.husnjak:igdb-api-jvm:1.0.11")//                IGDB API dependency
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Android
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
 
-    val room_version = "2.5.2"
+    // Project
+    implementation(libs.accompanist.permissions)
+    implementation(libs.appcompat)
+    implementation(libs.biometric)
+    implementation(libs.compose.destinations.core)
+    ksp(libs.compose.destinations.ksp)
+    implementation(libs.compose.ratingbar)
+    implementation(libs.compose.settings.m3)
+    implementation(libs.compose.settings.datastore)
+    implementation(libs.compose.shimmer)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.customactivityoncrash)
+    implementation(libs.datastore)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.gms.play.services.auth)
+    implementation(libs.gms.play.services.location)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.igdb.api.jvm)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.landscapist.animation)
+    implementation(libs.landscapist.glide)
+    implementation(libs.material.icons.extended)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    implementation(libs.security.crypto)
 
-    implementation("androidx.room:room-runtime:$room_version")
 
-    // To use Kotlin Symbol Processing (KSP)
-    ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
-
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
-
-    //Navigation library
-    implementation("io.github.raamcosta.compose-destinations:core:1.9.51")
-    ksp("io.github.raamcosta.compose-destinations:ksp:1.9.51")
-
-    implementation("com.github.TutorialsAndroid:crashx:v6.0.19")
-
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.47")
-    kapt("com.google.dagger:hilt-android-compiler:2.47")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-
-    implementation("com.google.accompanist:accompanist-permissions:0.30.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-
-    implementation("me.vponomarenko:compose-shimmer:1.0.0")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.gms:play-services-auth:20.6.0")
-
-    implementation("com.github.alorma:compose-settings-ui-m3:0.27.0")
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("com.github.alorma:compose-settings-storage-datastore:0.27.0")
-    implementation("com.github.MhmoudAlim:Compose-Ratingbar-library:0.1.0")
-
-    implementation("androidx.biometric:biometric:1.1.0")
+    // Test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+//    implementation("androidx.annotation:annotation:1.6.0")
+//    implementation("com.github.TutorialsAndroid:crashx:v6.0.19")
 }
 
 // Allow references to generated code
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,9 @@ val versionName by extra("0.1")
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
-
-    // Hilt
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.hiddenSecrets) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
