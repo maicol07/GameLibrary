4122469220a442dba52b4572aef963bd861f51a1
Simone Redighieri <simonedode10@gmail.com>
2023-08-19 10:35:27 +0200

    feat: Added home for publishers

---
+++ app/src/main/java/it/unibo/gamelibrary/ui/views/HomePublisher/HomePublisher.kt
@@ -0,0 +1,36 @@
+package it.unibo.gamelibrary.ui.views.HomePublisher
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import it.unibo.gamelibrary.ui.views.Home.HomeSection
+import it.unibo.gamelibrary.ui.views.Home.UserReview.UserReview
+import it.unibo.gamelibrary.utils.TopAppBarState
+
+@Composable
+fun HomePublisher(
+    navigator: DestinationsNavigator,
+    viewModel: HomePublisherViewModel = hiltViewModel()
+){
+    TopAppBarState.actions = { }
+    TopAppBarState.title = "Home"
+//TODO: re-fetch games and post when update
+    LazyColumn {
+        item {
+            HomeSection(
+                title = "My games",
+                viewModel.games,
+                navigator
+            )
+        }
+
+        items(
+            viewModel.posts,
+            key = { it.id })
+        {
+            UserReview(it, navigator)
+        }
+    }
+}
