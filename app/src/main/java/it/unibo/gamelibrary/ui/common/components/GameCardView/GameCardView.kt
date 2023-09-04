793665f758ba1493844ce51ff20916a4f2792157
Elia Bellavista <elia.bellavista@studio.unibo.it>
2023-09-04 16:02:38 +0200

    fix: merge import errors

--- app/src/main/java/it/unibo/gamelibrary/ui/common/components/GameCardView/GameCardView.kt
+++ app/src/main/java/it/unibo/gamelibrary/ui/common/components/GameCardView/GameCardView.kt
@@ -1,7 +1,9 @@
 package it.unibo.gamelibrary.ui.common.components.GameCardView
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,8 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
-import it.unibo.gamelibrary.ui.destinations.GameViewNavDestination
-import it.unibo.gamelibrary.ui.views.GameView.GameDetails
 import it.unibo.gamelibrary.ui.views.GameView.GameHeader
 import it.unibo.gamelibrary.ui.views.destinations.GameViewNavDestination
 import ru.pixnews.igdbclient.model.Game
